0bec3bfdb7c23eca7d67414ef0dce54cc20f7753
Rajan Singh <singhrajanbeawar1@gmail.com>
2026-08-06 23:00:39 +0530

    Fix: add missing newline at end of file in app.js

--- src/app.js
+++ src/app.js
@@ -20,4 +20,4 @@ app.use("/api/v1/users",userRoute);
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
-app.use(cookieParser())
+app.use(cookieParser());
