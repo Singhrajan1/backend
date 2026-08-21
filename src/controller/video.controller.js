2ae59f81f160472747945dfcf5314675ca0f2f8b
Rajan Singh <singhrajanbeawar1@gmail.com>
2026-08-21 21:58:40 +0530

    Add initial video controller structure: implement imports and placeholder comments

---
+++ src/controller/video.controller.js
@@ -0,0 +1,9 @@
+import { asyncHandler } from "../utils/asyncHandler";
+import { ApiError } from "../utils/apiError";
+import { ApiResponse } from "../utils/apiResponse";
+import { User } from "../model/user.model";
+
+//upload a video to the cloudinary using fs and multer
+//check whether the video is uploaded or not 
+//remove video
+//get All the video user has updated
