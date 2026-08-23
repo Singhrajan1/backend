1dffb3fcbfe976f546ba6da3b1ec4f965616acb4
Rajan Singh <singhrajanbeawar1@gmail.com>
2026-08-23 18:54:01 +0530

    Fix typo in ApiError references and ensure consistent error handling in auth middleware

---
+++ src/routes/video.route.js
@@ -0,0 +1,24 @@
+
+import { Router } from "express";
+import {
+  publishVideo,
+  getVideoById,
+  removeVideo,
+} from "../controller/video.controller.js";
+import { verifyUser } from "../middlewares/auth.middleware.js";
+import { upload } from "../middlewares/multer.middleware.js";
+
+const router = Router();
+
+router.route("/").post(
+  verifyUser,
+  upload.fields([
+    { name: "videoFile", maxCount: 1 },
+    { name: "thumbnail", maxCount: 1 },
+  ]),
+  publishVideo,
+);
+
+router.route("/:videoId").get(getVideoById).delete(verifyUser, removeVideo);
+
+export default router;
