59fdb6cb2b1d1d427b4b004f0b990b425529bf6b
Rajan Singh <singhrajanbeawar1@gmail.com>
2026-08-28 11:25:21 +0530

    Implement playlist controller methods: add userPlaylist, getAllPlaylist, and getPlaylistId; update playlist model to require name and fix videos field structure

--- src/controller/playlist.controller.js
+++ src/controller/playlist.controller.js
@@ -11,3 +11,75 @@
 // 10. Prevent the same video from being added multiple times
 // 11. Remove a video from a playlist
 // 12. Get playlist details along with its videos
+
+import { isValidObjectId } from "mongoose";
+import { Playlist } from "../model/playlist.model";
+import { ApiError } from "../utils/apiError";
+import { ApiResponse } from "../utils/apiResponse";
+import { asyncHandler } from "../utils/asyncHandler";
+
+const userPlaylist = asyncHandler(async (req, res) => {
+  const { name, description } = req.body;
+
+  if (!name || name.trim() === "") {
+    throw new ApiError(400, "name field in required to create a playlist");
+  }
+
+  const playlist = await Playlist.create({
+    name: name.trim(),
+    description: description?.trim() || "",
+    owner: req.user._id,
+  });
+
+  if (!playlist) {
+    throw new ApiError(
+      500,
+      "Something went wrong while creating the playlist!",
+    );
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, playlist, "Succussfully created a Playlist"));
+});
+
+const getAllPlaylist = asyncHandler(async (req, res) => {
+  const { userId } = req.params;
+
+  if (!isValidObjectId(userId)) {
+    throw new ApiError(400, "Invalid user Id");
+  }
+
+  const playlists = await Playlist.find({
+    owner: userId,
+  });
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, playlists, "Successfully fetched all the playlist"),
+    );
+});
+
+const getPlaylistId = asyncHandler(async (req, res) => {
+  const { playlistId } = req.params;
+
+  if (!isValidObjectId(playlistId)) {
+    throw new ApiError(400, "Invalid playlist Id");
+  }
+
+  const playlist = await Playlist.findById(playlistId).populate(
+    "owner",
+    "fullname username avatar",
+  ).populate("videos")
+
+  if(!playlist){
+    throw new ApiError(404,"Playlist does'nt exist")
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200,playlist,"Succussfully fetched the playlist"))
+});
+
+export { userPlaylist, getAllPlaylist, getPlaylistId };
