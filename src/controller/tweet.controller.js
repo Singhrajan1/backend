9ed7baab7982235db467574b107fdb2a04e19e8b
Rajan Singh <singhrajanbeawar1@gmail.com>
2026-09-02 23:56:45 +0530

    Refactor asyncHandler utility: remove commented-out code and clean up the implementation; add a note about gRPC learning in tweet controller

--- src/controller/tweet.controller.js
+++ src/controller/tweet.controller.js
@@ -4,3 +4,5 @@ import { ApiResponse } from "../utils/apiResponse";
 import { asyncHandler } from "../utils/asyncHandler";
 import { User } from "../model/user.model";
 import { Tweet } from "../model/tweets.model";
+
+//learned today about gRPC no work on controller today
